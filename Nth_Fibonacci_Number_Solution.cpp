47a5384d490113a36f30acb1cfa93760d5cde4ec
Anurag Singh <131255604+Untolders@users.noreply.github.com>
2023-08-13 15:37:40 +0530

    Create Nth_Fibonacci_Number_Solution.cpp

---
+++ Nth_Fibonacci_Number_Solution.cpp
@@ -0,0 +1,71 @@
+**********************************************************************  Question  ***********************************************************************************
+Given a positive integer n, find the nth fibonacci number. Since the answer can be very large, return the answer modulo 1000000007.
+
+Example 1:
+
+Input: 
+n = 2
+Output: 
+1 
+Explanation: 
+1 is the 2nd number of fibonacci series.
+Example 2:
+
+Input: 
+n = 5
+Output: 
+5
+Explanation: 
+5 is the 5th number of fibonacci series.
+Your Task:  
+You dont need to read input or print anything. Complete the function nthFibonacci() which takes n as input parameter and returns nth fibonacci number.
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(n)
+
+Constraints:
+1<= n <=105
+  
+  **********************************************************************  Solution  ***********************************************************************************
+
+  //{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int nthFibonacci(int n){
+        
+        int n1=0,n2=1,ans=0;
+         if(n == 0) return 0;
+        if(n == 1) return 1;
+     
+        for(int i=2; i<=n; i++)
+        {
+            ans = (n1 + n2) % 1000000007;
+            n1 = n2 % 1000000007;
+            n2 = ans;
+        }
+        return ans ;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.nthFibonacci(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+  
